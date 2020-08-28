6b57a7a64a5323d178a2b8336dfd6c85016b3571
Christopher Kim <chrisms91@gmail.com>
2020-08-28 01:52:41 -0700

    remove credentials

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://chrisms91:admin1234@boilerplate.m09q2.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+  // store information needed for registering into db
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
