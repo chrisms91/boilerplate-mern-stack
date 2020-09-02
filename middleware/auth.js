18e344e9e0c4c555b35a4221eff9d72f88af5719
Christopher Kim <chrisms91@gmail.com>
2020-09-01 23:12:22 -0700

    add logout route

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,12 @@ let auth = (req, res, next) => {
 
   // get token from client's cookie
   let token = req.cookies.x_auth;
-
+  console.log('token from cookie: ' + token);
   // decrypt token and find user
   User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user) return res.json({ isAuth: false, error: true });
-
+    console.log(user);
     req.token = token;
     req.user = user;
     next();
