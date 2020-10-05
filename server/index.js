4d4adc7d7e97c4be396a36787541f0112a5beabb
Christopher Kim <chrisms91@gmail.com>
2020-10-04 22:48:23 -0700

    update server

--- server/index.js
+++ server/index.js
@@ -7,13 +7,6 @@ const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
-// application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse various different custom JSON types as JSON
-app.use(bodyParser.json());
-app.use(cookieParser());
-
 const mongoose = require('mongoose');
 mongoose
   .connect(config.mongoURI, {
@@ -22,7 +15,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('MongoDB Connected...'))
+  .then(() => console.log('🚀 MongoDB Connected...'))
   .catch((err) => console.log(err));
 
 app.use(cors());
@@ -41,11 +34,10 @@ app.use('/api/users', require('./routes/users'));
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
   app.use(express.static('client/build'));
 
   // index.html for all page routes    html or routing and naviagtion
@@ -55,6 +47,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`Example app listening at localhost:${port}`);
+  console.log(`🚀 Server Listening on localhost://${port}`);
 });
